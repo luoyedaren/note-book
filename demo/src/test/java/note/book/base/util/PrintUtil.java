913e0870dff8dec1760e637092704c0307e9ddd6
chenghai <chenghai@fdc.com.cn>
2017-11-30 14:25:06 +0800

    add joda-time

---
+++ demo/src/test/java/note/book/base/util/PrintUtil.java
@@ -0,0 +1,12 @@
+package note.book.base.util;
+
+/**
+ * project: note-book
+ * Created by chenghai on 2017/11/30. - 星期四
+ */
+public class PrintUtil {
+
+	public static final void print(String s) {
+		System.out.println(s);
+	}
+}
---
+++ demo/src/test/java/note/book/joda/time/SimpleTimeTest.java
@@ -0,0 +1,31 @@
+package note.book.joda.time;
+
+import note.book.base.util.PrintUtil;
+import org.joda.time.*;
+import org.junit.Test;
+
+import java.util.Locale;
+
+
+/**
+ * project: note-book
+ * Created by chenghai on 2017/11/30. - 星期四
+ */
+public class SimpleTimeTest {
+
+	@Test
+	public void one() throws Exception {
+		// LocalDate
+		LocalDate localDate = LocalDate.now();
+		System.out.println(localDate.monthOfYear().getAsText());
+		System.out.println(localDate.monthOfYear().getAsShortText(Locale.ENGLISH));
+
+		// DateTime
+		DateTime dt = new DateTime();
+		DateTime year = dt.withYear(2000);
+		System.out.println(year);
+		DateTime plusHours = dt.plusHours(2);
+		System.out.println(plusHours);
+
+	}
+}
