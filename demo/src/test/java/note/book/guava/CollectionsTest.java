6c06e3bc1c144658c7d412f85543283c2ec26dd5
chenghai <chenghai@fdc.com.cn>
2018-01-17 17:31:31 +0800

    add 集合

---
+++ demo/src/test/java/note/book/guava/CollectionsTest.java
@@ -0,0 +1,25 @@
+package note.book.guava;
+
+import org.junit.Test;
+
+/**
+ * project: note-book
+ * Created by chenghai on 2018/1/17. - 星期三
+ * todo 待看
+ * 参考资料 ：https://www.ibm.com/developerworks/cn/java/j-lo-googlecollection/index.html
+ * <p>
+ * Immutable Collections: 还在使用 Collections.unmodifiableXXX() ？ Immutable Collections 这才是真正的不可修改的集合
+ * Multiset: 看看如何把重复的元素放入一个集合
+ * Multimaps: 需要在一个 key 对应多个 value 的时候 , 自己写一个实现比较繁琐 - 让 Multimaps 来帮忙
+ * BiMap: java.util.Map 只能保证 key 的不重复，BiMap 保证 value 也不重复
+ * MapMaker: 超级强大的 Map 构造类
+ * Ordering class: 大家知道用 Comparator 作为比较器来对集合排序，但是对于多关键字排序 Ordering class 可以简化很多的代码
+ * 其他特性
+ */
+public class CollectionsTest {
+
+	@Test
+	public void one() throws Exception {
+
+	}
+}
